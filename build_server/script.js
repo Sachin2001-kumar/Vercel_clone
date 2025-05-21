aa1c05240be66e771cc061b2df23d457dd8021e5
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-05-21 23:15:25 +0530

    add

--- build_server/script.js
+++ build_server/script.js
@@ -1,5 +1,21 @@
 // In this file i build the code and push the code into AWS s3
 
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+
+const S3Client = new S3Client({
+  region: "ap-south-1",
+  Credentials: {
+    accesskeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+const PROJECT_ID = "";
+var mime = require("mime-types");
+
 async function init() {
   console.log("Execution script.js");
   const outDirPath = path.join(__dirname, "output");
@@ -13,7 +29,26 @@ async function init() {
     console.log("Error", data.toString());
   });
 
-  p.on("close", function () {
+  p.on("close", async function () {
     console.log("build Complete");
+    const distfolderpath = path.join(__dirname, "output", "dist");
+    const distfolderContents = fs.readFileSync(distfolderpath, {
+      recursive: true,
+    });
+
+    for (const filepath of distfolderContents) {
+      if (fs.lstatSync(filepath).isDirectory()) continue;
+
+      const command = new PutObjectCommand({
+        Bucket: "",
+        Key: `__outputs/${PROJECT_ID}/${filepath}`,
+        Body: fs.createReadStream(filepath),
+        ContentType: mime.lookup(filepath),
+      });
+
+      await S3Client.send(command);
+    }
   });
 }
+
+init();
